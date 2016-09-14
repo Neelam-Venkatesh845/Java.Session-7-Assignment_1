64da50ec4178f7c878077b940f6f8effc3a61979
Neelam-Venaktesh <n.venkateshvvv@gmail.com>
2016-09-14 20:26:27 +0530

    Assignment 1 of Session 7 GitHub commit
    Assignment 1 of Session 7 GitHub commit

---
+++ Assignment1.java
@@ -0,0 +1,17 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		 
+	      try{
+	    	  int a[]=new int[5];
+
+	    	  a[7] = 9;
+	      }
+	      catch(ArrayIndexOutOfBoundsException e){
+	         System.out.println ("ArrayIndexOutOfBounds");
+	      }
+
+	}
+
+}
